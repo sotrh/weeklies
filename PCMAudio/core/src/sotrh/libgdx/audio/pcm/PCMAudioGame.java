6ad7f825a20132b16210eec951c19ff2305a415a
sotrh <bhbenjaminhansen@gmail.com>
2016-04-25 18:45:50 -0600

    Started libgdx audio synth attempt

--- PCMAudio/core/src/sotrh/libgdx/audio/pcm/PCMAudioGame.java
+++ PCMAudio/core/src/sotrh/libgdx/audio/pcm/PCMAudioGame.java
@@ -2,26 +2,34 @@ package sotrh.libgdx.audio.pcm;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.AudioDevice;
+import com.badlogic.gdx.audio.AudioRecorder;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class PCMAudioGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		AudioDevice playbackDevice = Gdx.audio.newAudioDevice(44100, true);
+		AudioRecorder recorder = Gdx.audio.newAudioRecorder(44100, true);
+
+		short[] samples = new short[44100 * 10]; // 10 seconds of mono audio
+		for (int i=0; i<samples.length; i++) {
+			samples[i] = (short) (i % 440);
+		}
+
+		recorder.read(samples, 0, samples.length);
+		playbackDevice.writeSamples(samples, 0, samples.length);
+		recorder.dispose();
+		playbackDevice.dispose();
+		Gdx.app.exit();
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void dispose() {
+
 	}
 }
